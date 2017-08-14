5bfdb93d97b3f55195e416cad490fb6b3049ab03
Brenton Poke <akioserano@hotmail.com>
2017-08-14 15:23:06 -0400

    Initial commit. Categories and Response class implemented

---
+++ src/main/java/com/CalendarResponse.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ *
+ * @author Brenton
+ */
+
+public class CalendarResponse {
+    @JsonProperty("calendarShortname")
+    String Shortname;
+    @JsonProperty("name")
+   private String FullName;
+   private String background;
+   private String image;
+   private List<Category> categories;
+   private String error;
+
+
+    public String getError() {
+        return error;
+    }
+
+
+    public String getShortname() {
+        return Shortname;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public String getBackground() {
+        return background;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+}
---
+++ src/main/java/com/Category.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+import com.fasterxml.jackson.annotation.JsonProperty;
+/**
+ *
+ * @author Brenton
+ */
+
+class Category {
+    String name;
+    String categoryShorname;
+    String calendarShortname;
+    boolean rolling;
+    boolean geoEnabled;
+    @JsonProperty("private")
+    boolean isPrivate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategoryShorname() {
+        return categoryShorname;
+    }
+
+    public void setCategoryShorname(String categoryShorname) {
+        this.categoryShorname = categoryShorname;
+    }
+
+    public String getCalendarShortname() {
+        return calendarShortname;
+    }
+
+    public void setCalendarShortname(String calendarShortname) {
+        this.calendarShortname = calendarShortname;
+    }
+
+    public boolean isRolling() {
+        return rolling;
+    }
+
+    public void setRolling(boolean rolling) {
+        this.rolling = rolling;
+    }
+
+    public boolean isGeoEnabled() {
+        return geoEnabled;
+    }
+
+    public void setGeoEnabled(boolean geoEnabled) {
+        this.geoEnabled = geoEnabled;
+    }
+
+    public boolean isIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+    
+    
+}
